b3f9278692555a42c9998628faaf93ed557f7078
Gabriel <gatoumix@gmail.com>
2022-07-18 19:13:55 -0300

    react query done, pagination done, to-do: filtros e estilizar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import './App.css';
 import { CssBaseline } from '@mui/material';
-import { QueryClientProvider, useQuery } from 'react-query';
-import axios from 'axios';
+import Heroi from './Components/Heroi';
 function App() {
-  const { data, isFetching } = useQuery(
-    'herois',
-    async () => {
-      const response = await axios.get(
-        'https://gateway.marvel.com:443/v1/public/characters?apikey=175433ccb801e487f623da18c023d07e',
-      );
-      return response.data;
-    },
-    {
-      refetchOnWindowFocus: false,
-      staleTime: 60 * 1000 * 60, // 1 hora
-    },
-  );
-
   return (
     <main>
       <CssBaseline />
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route
-            path={'/'}
-            element={<Home data={data} isFetching={isFetching} />}
-          />
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/heroi/:id'} element={<Heroi />} />
         </Routes>
+
         <Footer />
       </BrowserRouter>
     </main>
