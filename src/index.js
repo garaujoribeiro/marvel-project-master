d0a12249e251aeae5a5d82520c4f7c984b992d43
Gabriel <gatoumix@gmail.com>
2022-07-20 11:33:30 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { queryClient } from './QueryClient/QueryClient';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
     <App />
-    <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>,
 );
